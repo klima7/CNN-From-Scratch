9f6914e252a83514e71d8a932a0a3865ae92e6d2
Lukasz Klimkiewicz <ukasz.klimkiewicz@gmail.com>
2022-12-06 22:31:53 +0100

    tmp

--- nn/layers/conv.py
+++ nn/layers/conv.py
@@ -40,7 +40,10 @@ class BaseConvLayer(Layer, ABC):
 
     @property
     def output_slice_size(self):
-        return (self.input_slice_size - self.dilated_kernel_size + 1) // self.stride
+        start = self.dilated_kernel_size // 2
+        end = self.input_slice_size - start - 1
+        diff = end - start + 1
+        return np.ceil(diff / self.stride).astype(int)
 
     def get_output_shape(self):
         return tuple((*self.output_slice_size, self.output_slices_count))
@@ -101,30 +104,51 @@ class Conv2DLayer(BaseConvLayer):
         return len(input_shape) == 3
 
     def propagate(self, x):
-        slices = [self.__conv_with_kernel(x, kernel) for kernel in self.kernels]
-        slices = np.array(slices)
-        output = np.moveaxis(slices, [0, 1, 2], [2, 0, 1])
-        return output
+        image_sections = self.__get_sections(x)
+        convoluted = self.__conv_sections(image_sections, self.kernels)
+        return convoluted
 
     def backpropagate(self, delta):
-        raise NotImplementedError
+        for slice_no in range(self.input_slices_count):
+            slice_delta = delta[..., slice_no]
 
-    def __conv_with_kernel(self, x, kernel):
-        output = np.zeros(self.output_slice_size, dtype=x.dtype)
-        positions0 = [self.dilated_kernel_size[0] // 2 + i * self.stride[0] for i in range(self.output_slice_size[0])]
-        positions1 = [self.dilated_kernel_size[1] // 2 + i * self.stride[1] for i in range(self.output_slice_size[1])]
-        for index0, pos0 in enumerate(positions0):
-            for index1, pos1 in enumerate(positions1):
-                output[index0][index1] = self.__conv(x, kernel, (pos0, pos1))
-        return output
+    def __conv_tensor(self, tensor, kernels):
+        sections = self.__get_sections(tensor)
+        flatten_kernels = kernels.reshape((len(kernels), -1))
+        flatten_convoluted = sections @ flatten_kernels.T
+        convoluted = flatten_convoluted.reshape((*self.output_slice_size, len(kernels)))
 
-    def __conv(self, x, kernel, pos):
+    def __conv_sections(self, sections, kernels):
+
+        return convoluted
+
+    def __get_sections(self, data):
+        sections_count = np.prod(self.output_slice_size)
+        kernel_count = np.prod(self.kernel_size)
+        sections = np.zeros((sections_count, kernel_count * self.input_slices_count))
+
+        centers0 = [self.dilated_kernel_size[0] // 2 + i * self.stride[0] for i in range(self.output_slice_size[0])]
+        centers1 = [self.dilated_kernel_size[1] // 2 + i * self.stride[1] for i in range(self.output_slice_size[1])]
+
+        linear_index = 0
+        for center0 in centers0:
+            for center1 in centers1:
+                sections[linear_index] = self.__get_single_image_section(data, (center0, center1))
+                linear_index += 1
+
+        return sections
+
+    def __get_single_image_section(self, data, pos):
+        section = np.zeros((np.prod(self.kernel_size), self.input_slices_count))
         kernel_half = self.kernel_size // 2
-        result = 0
-        x_indexes0 = [pos[0] + self.dilation[0] * i for i in range(-kernel_half[0], kernel_half[0]+1)]
-        x_indexes1 = [pos[1] + self.dilation[1] * i for i in range(-kernel_half[1], kernel_half[1] + 1)]
-        for kernel_index0, x_index0 in enumerate(x_indexes0):
-            for kernel_index1, x_index1 in enumerate(x_indexes1):
-                for slice_index in range(self.input_slices_count):
-                    result += x[x_index0][x_index1][slice_index] * kernel[kernel_index0][kernel_index1][slice_index]
-        return result
+
+        positions0 = [pos[0] + self.dilation[0] * i for i in range(-kernel_half[0], kernel_half[0]+1)]
+        positions1 = [pos[1] + self.dilation[1] * i for i in range(-kernel_half[1], kernel_half[1] + 1)]
+
+        linear_index = 0
+        for pos0 in positions0:
+            for pos1 in positions1:
+                section[linear_index] = data[pos0, pos1, :]
+                linear_index += 1
+
+        return section.flatten()
