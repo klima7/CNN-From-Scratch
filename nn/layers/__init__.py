43a3e5e13ab6d565950be10a46868c74580bb8d2
Lukasz Klimkiewicz <ukasz.klimkiewicz@gmail.com>
2022-12-09 18:14:29 +0100

    Remove 1D padding layer

--- nn/layers/__init__.py
+++ nn/layers/__init__.py
@@ -5,6 +5,6 @@ from .flatten import FlattenLayer
 from .input import InputLayer
 from .pool import Pool1DLayer, Pool2DLayer
 from .log import Log1DLayer, Log2DLayer
-from .padding import Padding1DLayer, Padding2DLayer
+from .padding import Padding2DLayer
 from .bias import BiasLayer
 from .dropout import DropoutLayer
--- nn/layers/padding.py
+++ nn/layers/padding.py
@@ -1,17 +1,15 @@
-from abc import ABC
-
 import numpy as np
 
 from .base import Layer
 from ..exceptions import InvalidParameterException
 
 
-class BasePaddingLayer(Layer, ABC):
+class Padding2DLayer(Layer):
 
-    def __init__(self, mode, padding_size):
+    def __init__(self, padding_size, mode='zeros'):
         super().__init__()
         self.mode = mode
-        self.padding_size = np.array(padding_size)
+        self.padding_size = self._get_unified_padding_size(padding_size)
 
     @property
     def slices_count(self):
@@ -26,54 +24,43 @@ class BasePaddingLayer(Layer, ABC):
         if self.mode == 'valid':
             return self.input_slice_size
         else:
-            return self.input_slice_size + 2*self.padding_size
-
-    def get_output_shape(self):
-        return tuple((*self.output_slice_size, self.slices_count))
-
-    def _apply_padding(self, array, axes_pad_width):
-        sym_pad_widths = [(val, val) for val in axes_pad_width]
-        if self.mode == 'valid':
-            return np.array(array)
-        elif self.mode == 'zeros':
-            return np.pad(array, sym_pad_widths, mode='constant', constant_values=0)
-        elif self.mode == 'edge':
-            return np.pad(array, sym_pad_widths, mode='edge')
-        else:
-            raise InvalidParameterException(f'Invalid padding mode: {self.mode}')
-
-    @staticmethod
-    def _remove_padding(array, axes_pad_width):
-        reversed_padding = []
-        for pad_width, dim in zip(axes_pad_width, array.shape):
-            reversed_padding.append(slice(pad_width, dim - pad_width))
-        return array[tuple(reversed_padding)]
-
-
-class Padding1DLayer(BasePaddingLayer):
-
-    def __init__(self, mode, padding_size=1):
-        super().__init__(mode, padding_size)
-
-    def is_input_shape_valid(self, input_shape):
-        return len(input_shape) == 2
-
-    def propagate(self, x):
-        return self._apply_padding(x, [self.padding_size, 0])
-
-    def backpropagate(self, delta):
-        return self._remove_padding(delta, [self.padding_size, 0])
-
-
-class Padding2DLayer(BasePaddingLayer):
-    def __init__(self, mode, padding_size=(1, 1)):
-        super().__init__(mode, padding_size)
+            return self.input_slice_size + np.array([sum(self.padding_size[0]), sum(self.padding_size[1])])
 
     def is_input_shape_valid(self, input_shape):
         return len(input_shape) == 3
 
+    def get_output_shape(self):
+        return tuple((*self.output_slice_size, self.slices_count))
+
     def propagate(self, x):
-        return self._apply_padding(x, [*self.padding_size, 0])
+        return self._apply_padding(x, [*self.padding_size, (0, 0)])
 
     def backpropagate(self, delta):
-        return self._remove_padding(delta, [*self.padding_size, 0])
+        return self._remove_padding(delta, [*self.padding_size, (0, 0)])
+
+    @staticmethod
+    def _get_unified_padding_size(size):
+        if isinstance(size, int):
+            return (size, size), (size, size)
+        elif isinstance(size, tuple):
+            if isinstance(size[0], int):
+                return (size[0], size[0]), (size[1], size[1])
+            elif isinstance(size[0], tuple):
+                return size
+
+    def _apply_padding(self, array, axes_pad_width):
+        if self.mode == 'valid':
+            return np.array(array)
+        elif self.mode == 'zeros':
+            return np.pad(array, axes_pad_width, mode='constant', constant_values=0)
+        elif self.mode == 'edge':
+            return np.pad(array, axes_pad_width, mode='edge')
+        else:
+            raise InvalidParameterException(f'Invalid padding mode: {self.mode}')
+
+    @staticmethod
+    def _remove_padding(array, paddings):
+        reversed_padding = []
+        for padding, dim in zip(paddings, array.shape):
+            reversed_padding.append(slice(padding[0], dim - padding[1]))
+        return array[tuple(reversed_padding)]
