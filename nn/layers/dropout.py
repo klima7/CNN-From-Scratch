a44d83248f3789cf45b4bb2bebb0d077f6516eed
klima7 <ukasz.klimkiewicz@gmail.com>
2022-12-10 14:11:33 +0100

    Refactor

--- nn/layers/dropout.py
+++ nn/layers/dropout.py
@@ -8,7 +8,7 @@ class DropoutLayer(Layer):
     def __init__(self, drop_rate=0.5):
         super().__init__()
         self.drop_rate = drop_rate
-        self.drop_mask = None
+        self.__drop_mask = None
         self.__scale_factor = 1 / (1-self.drop_rate)
 
     def get_output_shape(self):
@@ -18,15 +18,15 @@ class DropoutLayer(Layer):
         if not self.nn.training:
             return x
 
-        self.drop_mask = self.__create_drop_mask(x.shape)
+        self.__drop_mask = self.__create_drop_mask(x.shape)
         new_x = np.array(x)
-        new_x[self.drop_mask] = 0
+        new_x[self.__drop_mask] = 0
         new_x *= self.__scale_factor
         return new_x
 
     def backpropagate(self, delta):
         new_delta = np.array(delta)
-        new_delta[self.drop_mask] = 0
+        new_delta[self.__drop_mask] = 0
         new_delta *= self.__scale_factor
         return new_delta
 
