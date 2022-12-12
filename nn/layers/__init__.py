d5c6ce261ee1aa72f7ec96fabc1da1f85a8ece98
Lukasz Klimkiewicz <ukasz.klimkiewicz@gmail.com>
2022-12-12 18:32:32 +0100

    Remove Log layer

--- nn/layers/__init__.py
+++ nn/layers/__init__.py
@@ -4,7 +4,6 @@ from .dense import DenseLayer
 from .flatten import FlattenLayer
 from .input import InputLayer
 from .pool import Pool2DLayer
-from .log import Log1DLayer, Log2DLayer
 from .padding import Padding2DLayer
 from .bias import BiasLayer
 from .dropout import DropoutLayer
