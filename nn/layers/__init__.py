4197ce69ab37b7f101af266a502d9fc0e27aae67
Lukasz Klimkiewicz <ukasz.klimkiewicz@gmail.com>
2022-12-14 22:48:30 +0100

    Add simple pooling layer

--- nn/layers/__init__.py
+++ nn/layers/__init__.py
@@ -3,7 +3,7 @@ from .conv import Conv2DLayer
 from .dense import DenseLayer
 from .flatten import FlattenLayer
 from .input import InputLayer
-from .pool import Pool2DLayer
+from .pool import Pool2DLayer, SimplePool2DLayer
 from .padding import Padding2DLayer
 from .bias import BiasLayer
 from .dropout import DropoutLayer
--- nn/layers/pool.py
+++ nn/layers/pool.py
@@ -82,3 +82,58 @@ class Pool2DLayer(Layer):
             return np.min, np.argmin
         else:
             raise InvalidParameterException(f'Invalid pool variant: {variant}')
+
+
+class SimplePool2DLayer(Layer):
+
+    def __init__(self, pool_size, variant='max'):
+        super().__init__()
+
+        self.pool_size = np.broadcast_to(pool_size, (2,))
+        self.variant = variant
+        self.pool_function = self.__get_pool_function(variant)
+
+    @property
+    def slices_count(self):
+        return self.input_shape[-1]
+
+    @property
+    def input_slice_size(self):
+        return self.input_shape[:-1]
+
+    @property
+    def output_slice_size(self):
+        return (self.input_slice_size - self.pool_size) // self.pool_size + 1
+
+    def get_output_shape(self):
+        return tuple((*self.output_slice_size, self.slices_count))
+
+    def validate_input_shape(self):
+        if len(self.input_shape) != 3:
+            raise InvalidShapeException(f'{self.__class__.__name__} input must be 3D')
+
+        if self.input_shape[0] % self.pool_size[0] != 0 or self.input_shape[1] % self.pool_size[1] != 0:
+            msg = f'{self.__class__.__name__} input shape {self.input_shape[:2]} must be dividable by pool size {self.pool_size}'
+            raise InvalidShapeException(msg)
+
+    def propagate(self, x):
+        output = np.zeros(self.output_shape, dtype=x.dtype)
+        for i in range(output.shape[0]):
+            for j in range(output.shape[1]):
+                i_slice = np.s_[i * self.pool_size[0]:(i + 1) * self.pool_size[0]]
+                j_slice = np.s_[j * self.pool_size[1]:(j + 1) * self.pool_size[1]]
+                group = x[i_slice, j_slice, :]
+                output[i, j, :] = self.pool_function(group, axis=(0, 1))
+        return output
+
+    def backpropagate(self, delta):
+        return np.repeat(np.repeat(delta, self.pool_size[0], axis=0), self.pool_size[1], axis=1)
+
+    @staticmethod
+    def __get_pool_function(variant):
+        if variant == 'max':
+            return np.max
+        elif variant == 'avg':
+            return np.mean
+        else:
+            raise InvalidParameterException(f'Invalid pool variant: {variant}')
