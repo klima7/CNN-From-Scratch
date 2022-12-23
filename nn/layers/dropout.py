c2f5f6a4310bb806e63adc209055acf320f3e9d4
Lukasz Klimkiewicz <ukasz.klimkiewicz@gmail.com>
2022-12-23 11:25:14 +0100

    Fix dropout layer

--- nn/layers/dropout.py
+++ nn/layers/dropout.py
@@ -8,7 +8,7 @@ class DropoutLayer(Layer):
     def __init__(self, drop_rate=0.5):
         super().__init__()
         self.drop_rate = drop_rate
-        self.__drop_mask = None
+        self.__keep_mask = None
         self.__scale_factor = 1 / (1-self.drop_rate)
 
     def get_output_shape(self):
@@ -18,17 +18,8 @@ class DropoutLayer(Layer):
         if not self.nn.training:
             return x
 
-        self.__drop_mask = self.__create_drop_mask(x.shape)
-        new_x = np.array(x)
-        new_x[self.__drop_mask] = 0
-        new_x *= self.__scale_factor
-        return new_x
+        self.__keep_mask = np.random.binomial(1, 1-self.drop_rate, size=x.shape)
+        return x * self.__keep_mask * self.__scale_factor
 
     def backpropagate(self, delta):
-        new_delta = np.array(delta)
-        new_delta[self.__drop_mask] = 0
-        new_delta *= self.__scale_factor
-        return new_delta
-
-    def __create_drop_mask(self, shape):
-        return np.random.rand(*shape) < self.drop_rate
+        return delta * self.__keep_mask * self.__scale_factor
