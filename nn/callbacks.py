5a75d2e41e97ef27f45e633efaacdf5df6904479
Lukasz Klimkiewicz <ukasz.klimkiewicz@gmail.com>
2022-12-24 16:14:47 +0100

    Add ModelCheckpoint callback

--- nn/callbacks.py
+++ nn/callbacks.py
@@ -25,28 +25,44 @@ class Callback:
         pass
 
 
-class TestCallback:
+class ModelCheckpoint(Callback):
 
-    def __init__(self):
-        self.model = None
+    def __init__(self, filepath, monitor='val_loss', mode='min', save_best_only=False, verbose=False):
+        super().__init__()
+        self.filepath = filepath
+        self.monitor = monitor
+        self.mode = mode
+        self.save_best_only = save_best_only
+        self.verbose = verbose
 
-    def set_model(self, model):
-        self.model = model
-
-    def on_epoch_begin(self):
-        print('on_epoch_begin')
-
-    def on_epoch_end(self):
-        print('on_epoch_end')
-
-    def on_validation_begin(self):
-        print('on_validation_begin')
-
-    def on_validation_end(self):
-        print('on_validation_end')
+        self.best_metric = None
 
     def on_train_begin(self):
-        print('on_train_begin')
+        self.best_metric = self.__get_metric()
+        self.__save()
 
-    def on_train_end(self):
-        print('on_train_end')
+    def on_epoch_end(self):
+        if self.save_best_only:
+            metric = self.__get_metric()
+            if self.__is_metric_better(metric):
+                self.__save()
+                self.best_metric = metric
+        else:
+            self.__save()
+
+    def __save(self):
+        self.model.save(self.filepath)
+        if self.verbose:
+            print(f'Saving model to {self.filepath}')
+
+    def __get_metric(self):
+        history = self.model.history
+        return history[self.monitor][-1] if self.monitor in history else None
+
+    def __is_metric_better(self, metric):
+        if self.best_metric is None:
+            return True
+        if self.mode == 'min':
+            return metric < self.best_metric
+        elif self.mode == 'max':
+            return metric > self.best_metric
