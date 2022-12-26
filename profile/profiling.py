f7a44c4c261017055529c6800aa54c9681a65af1
Lukasz Klimkiewicz <ukasz.klimkiewicz@proton.me>
2022-12-26 11:57:59 +0100

    Remove profile directory

--- profile/profiling.py
+++
@@ -1,8 +0,0 @@
-import cProfile
-import subprocess
-import code
-
-code.prepare(n_samples=1000)
-code.start()
-cProfile.run('code.start()', filename='output.prof')
-subprocess.run('snakeviz output.prof')
